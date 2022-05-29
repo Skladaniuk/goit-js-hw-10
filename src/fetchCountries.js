ba52eb9c51e145aa1ede13266fdaf9d9880503b1
Skladaniuk <skladanyuk1999@gmail.com>
2022-05-29 20:50:51 +0300

    fixed bugs

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,25 +1,11 @@
-// export default function fetchCountries(name) { 
-
-//     return fetch(`https://restcountries.com/v3.1/name/{name}$fields=name,capital,population,flags,languages`).then(
-//         responce => {
-//             if (!responce.ok) {
-//                 throw new Error(responce.status);
-//             }
-//             return responce.json();
-//         },
-//     );
-
-
-// }
 export default function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v3.1/';
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(responce => {
+    if (!responce.ok) {
+      throw new Error(responce.status);
+    }
 
-  return fetch(`${BASE_URL}name/${name}?fields=name,capital,population,flags,languages`).then(
-    response => {
-      if (!response.ok) {
-          throw new Error(response.status);
-      }
-      return response.json();
-    },
-  );
+    return responce.json();
+  });
 }
